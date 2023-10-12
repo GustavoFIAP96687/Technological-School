3cd0add39a6213fa932299d4c18b8df0dbbf4870
GustavoFIAP96687 <rm96687@fiap.com.br>
2023-10-12 15:12:06 -0300

    Atualizado

--- front-end/src/app/categorias/new/paje.jsx
+++ front-end/src/app/categorias/new/paje.jsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { create } from "@/actions/categorias";
+import ComboBoxIconeCategoria from "@/components/combobox-icone-categoria";
+import { useState } from "react";
 import Button from "@/components/button";
 import InputText from "@/components/input-text";
 import NavBar from "@/components/navbar";
@@ -9,12 +11,14 @@ import toast from "react-hot-toast";
 
 export default function CategoriaNew() {
     const { push } = useRouter()
+    const [messages, setMessages] = useState([])
 
     async function onSubmit(formData){
         const resp = await create(formData)
 
         if (resp?.error) {
             toast.error(resp.error)
+            setMessages(resp.messages)
             return
         }
 
@@ -29,10 +33,10 @@ export default function CategoriaNew() {
                 <h2>Cadastrar Categoria</h2>
 
                 <form action={onSubmit} className="flex flex-col items-start gap-2 mt-2">
-                    <InputText name="icone" label="ícone" />
+                    <ComboBoxIconeCategoria />
                     <InputText name="nome" label="nome" />
+                    <span className="text-red-400">{messages.find(m => m.field === "nome")?.message}</span>
                     <Button type="button">salvar</Button>
-
                 </form>
 
 
